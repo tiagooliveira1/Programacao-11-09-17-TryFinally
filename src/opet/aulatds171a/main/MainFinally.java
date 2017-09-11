20adc0be9c1203e380022eae2fb08f6c6604caa5
Tiago Oliveira <tiago@econtabil.com>
2017-09-11 19:35:46 -0300

    First Commint

--- src/opet/aulatds171a/main/MainFinally.java
+++ src/opet/aulatds171a/main/MainFinally.java
@@ -3,22 +3,20 @@ package opet.aulatds171a.main;
 public class MainFinally {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			System.out.println("Teste");
-			int i=0, j=0;
+			int i = 0, j = 0;
 			// força erro de arythmetic expression
-			i = i/j;
+			i = i / j;
 		} catch (ArithmeticException e) {
-			System.out.println("Deu exception");
+			System.out.println("Deu exception.");
 			throw e;
-		}
-		finally {
-			//finally sempre executa, independente se try levantou excessão ou não 
+		} finally {
+			// finally sempre executa, independente se try levantou excessão ou
+			// não
 			System.out.println("Passou no bloco try");
 		}
- 
-
 
 	}
 
